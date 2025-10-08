2dffa63b421a2adc304056dc0ea26ac3ce5699b7
Szymon Talaga <stalaga@protonmail.com>
2025-10-08 14:30:16 +0200

    Reimplement 'make_dataset.py'.

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -5,11 +5,11 @@ from newsuse.data import DataFrame
 
 from project import config, paths
 
-# %% Get raw data ------------------------------------------------------------------------
+# %% Get raw data --------------------------------------------------------------------
 
-data = DataFrame.from_(paths.posts)
+posts = DataFrame.from_(paths.posts)
 
-# %% Compute data quality statistics -----------------------------------------------------
+# %% Compute data quality statistics -------------------------------------------------
 
 quality = (
     DataFrame.from_(paths.daily)
@@ -22,46 +22,40 @@ quality = (
 keys = ["country", "name"]
 cols = ["reactions", "comments", "shares"]
 
-Q = data.groupby(keys)[cols].quantile(q=np.arange(1, 10) / 10)
+Q = posts.groupby(keys)[cols].quantile(q=np.arange(1, 10) / 10)
 M = (
     Q.groupby(level=keys)[cols]
     .agg(lambda s: (((X := s.to_numpy())[1:] - X[:-1]) / (1 + X[:-1])).max())
     .sort_values("shares", ascending=False)
 )
 
+# %% ---------------------------------------------------------------------------------
+
 quality = (
-    quality.merge(M, left_index=True, right_index=True)
+    quality.set_index(keys)
+    .merge(M, left_index=True, right_index=True)
+    .reset_index()
     .assign(
         bad_rate=lambda df: df["rate"] < config.dataset.min_posts_per_day,
         bad_shares=lambda df: df["shares"] > config.dataset.max_decile_difference,
     )
     .assign(bad=lambda df: df["bad_rate"] | df["bad_shares"])
-    .reset_index(names="pid")
+    .set_index(keys)
 )
 
-# %% Get labels --------------------------------------------------------------------------
+# %% Make dataset --------------------------------------------------------------------
 
-labels = DataFrame.from_(paths.labels).drop_duplicates("key", ignore_index=True)
-
-# %% Make dataset ------------------------------------------------------------------------
-
-dataset = data.query(f"~pid.isin({quality.query('bad').pid.tolist()})").merge(
-    labels, how="left", on="key"
+dataset = (
+    posts.set_index(keys)
+    .pipe(lambda df: df[~df.index.isin(quality.query("bad").index)])
+    .set_index("key", append=True)
+    .reset_index(keys)
+    .reset_index()
 )
 
-# %% Determine time bounds ---------------------------------------------------------------
-
-start, end = (
-    dataset.groupby(["country"])["timestamp"]
-    .agg(["min", "max"])
-    .agg({"min": "max", "max": "min"})
-)
-
-dataset = dataset[dataset["timestamp"].between(start, end)]
-
-# %% Save dataset, quality, and bad sources ----------------------------------------------
+# %% Save dataset, quality, and bad sources ------------------------------------------
 
 dataset.to_(paths.dataset)
 quality.to_(paths.quality)
 
-# %% -------------------------------------------------------------------------------------
+# %% ---------------------------------------------------------------------------------
