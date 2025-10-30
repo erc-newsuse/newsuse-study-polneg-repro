4ae27d061319fadbedb9ad5796f44e9c17c8bdd0
Szymon Talaga <stalaga@protonmail.com>
2025-10-31 00:42:28 +0100

    Simplify 'stages/ml/train_negativity.py'.

--- stages/ml/train_negativity.py
+++ stages/ml/train_negativity.py
@@ -1,8 +1,7 @@
 # %% Setup ---------------------------------------------------------------------------
 
 import datasets
-from newsuse.data import DataFrame
-from transformers import AutoConfig, AutoTokenizer, Trainer
+from transformers import AutoTokenizer, Trainer
 
 from project import config, paths
 from project.model import (
@@ -17,32 +16,8 @@ domain = "negativity"
 
 # %% ---------------------------------------------------------------------------------
 
-params = (
-    DataFrame.from_(paths.proc / f"hyper-{domain}.parquet")
-    .pipe(
-        lambda df: df.loc[
-            df.value.idxmax(), [c for c in df.columns if c.startswith("params_")]
-        ]
-    )
-    .rename(lambda s: s.removeprefix("params_"))
-    .rename({"shared_n_layers": "num_shared_layers", "head_n_layers": "num_head_layers"})
-    .to_dict()
-)
-
-model_params = {
-    k: v
-    for k, v in params.items()
-    if k in NewsuseNegativityClassifierConfig.__init__.__annotations__
-}
-trainer_params = {k: v for k, v in params.items() if k not in model_params}
-
-# %% ---------------------------------------------------------------------------------
-
-model_config = config.ml.models[domain][model_params.pop("base")]
-base_config = AutoConfig.from_pretrained(model_config.checkpoint, **model_config.base)
-model_config = NewsuseNegativityClassifierConfig(
-    base_config, **{**model_config.newsuse, **model_params}
-)
+base_model_name = "distilbert/distilbert-base-multilingual-cased"
+model_config = NewsuseNegativityClassifierConfig(base_model_name)
 model = NewsuseNegativityClassifier(model_config)
 tokenizer = AutoTokenizer.from_pretrained(model_config.base_name_or_path)
 
