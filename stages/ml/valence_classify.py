4f4c3cfffbddd3e1316d396a7755513c51b678e5
Szymon Talaga <stalaga@protonmail.com>
2025-12-09 14:53:08 +0100

    Prepare before testing simplified model in Bambi.

--- stages/ml/valence_classify.py
+++ stages/ml/valence_classify.py
@@ -1,5 +1,6 @@
 # %% ---------------------------------------------------------------------------------
 
+import numpy as np
 import pandas as pd
 from newsuse.data import DataFrame
 from tqdm.auto import tqdm
@@ -40,25 +41,38 @@ pipe = pipeline("text-multi-classification", model=model, tokenizer=tokenizer)
 
 # %% ---------------------------------------------------------------------------------
 
+data = data.sample(n=100000, random_state=303)
 dset = KeyDataset(data[["text"]], "text")
 bsize = config.ml.inference.batch_size
-results = DataFrame(tqdm(pipe(dset, batch_size=bsize), total=len(dset)))
+results = [*tqdm(pipe(dset, top_k=None, batch_size=bsize), total=len(dset))]
 
 # %% ---------------------------------------------------------------------------------
 
-output = (
-    pd.concat(
-        [
-            DataFrame(results[t].tolist()).rename(
-                columns={"label": t, "score": f"{t}_score"}
-            )
-            for t in targets
-        ],
-        axis=1,
-    )
-    .set_index(data.index)
-    .reset_index()
-)
+
+def make_record(result: dict) -> dict:
+    def make_label(label: int) -> str:
+        return "negative" if label == -1 else "neutral" if label == 0 else "positive"
+
+    def _make(target: str) -> dict:
+        scores = {
+            f"{target}_p_{make_label(r['label'])}": r["score"] for r in result[target]
+        }
+        idx = np.argmax([r["score"] for r in result[target]])
+        return {target: result[target][idx]["label"], **scores}
+
+    record = {}
+    for t in targets:
+        record.update(_make(t))
+    return record
+
+
+# %% ---------------------------------------------------------------------------------
+
+records = DataFrame([make_record(r) for r in results])
+
+# %% ---------------------------------------------------------------------------------
+
+output = records.set_index(data.index).reset_index()
 
 # %% ---------------------------------------------------------------------------------
 
