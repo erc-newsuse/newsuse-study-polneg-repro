ef2a81daba12abb95c1d052448297fe34a86e30a
Szymon Talaga <stalaga@protonmail.com>
2025-12-10 02:32:17 +0100

    Add well designed ADVI for sentiment.

--- analyses/glmm/valence/sentiment.py
+++ analyses/glmm/valence/sentiment.py
@@ -38,6 +38,9 @@ political = dict(enumerate(config.categorical.political))
 idata = az.from_netcdf(paths.glmm / "valence" / f"{target}.nc")
 idata = set_xindex(idata, [opts.index_col, *sum(opts.predictors.values(), start=[])])
 epred = az.extract(idata, group="posterior_epred")
+# Average out week effects to focus on main effects
+if "weekend" in epred.coords:
+    epred = (epred.sel(weekend=0) + epred.sel(weekend=1)) / 2
 
 # %% ---------------------------------------------------------------------------------
 
