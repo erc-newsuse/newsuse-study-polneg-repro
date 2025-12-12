9bbe8b66d5a5a83dcc13be7520d881ccb5c5efa5
Szymon Talaga <stalaga@protonmail.com>
2025-12-12 11:02:55 +0100

    Implement fitting of all valence model via a single script depending on 'NEWSUSE_POLNEG_GLMM_VALENCE_TARGET' environmental variable.

--- stages/glmm/valence.py
+++ stages/glmm/valence.py
@@ -1,77 +1,78 @@
 # %% ---------------------------------------------------------------------------------
 
+import os
 
-import bambi as bmb
+import arviz as az
 import numpy as np
 import pandas as pd
 from newsuse.data import DataFrame
+from omegaconf import OmegaConf
 
 from project import config, paths
-from project.inference import advi_trace_to_inference, make_priors
+from project.brms import brm, ro
 
-target = "valence"
-output_dir = paths.glmm / "valence"
-output_dir.mkdir(parents=True, exist_ok=True)
+az.rcParams.update(config.arviz)
 
-opts = config.glmm.valence[target]
+# %% ---------------------------------------------------------------------------------
+
+target = os.environ.get("NEWSUSE_POLNEG_GLMM_VALENCE_TARGET", "event")
+opts = config.glmm.valence.targets[target]
+
+dirpath = paths.glmm / "valence"
+dirpath.mkdir(parents=True, exist_ok=True)
+
 rng = np.random.default_rng(opts.seed)
 
 # %% ---------------------------------------------------------------------------------
 
-data = (
-    DataFrame.from_(paths.final)
-    .pipe(
-        lambda df: df.assign(
-            political=pd.Categorical(df["political"], **config.categorical.political),
-            event=pd.Categorical(df["event"], **config.categorical.valence),
-            sentiment=pd.Categorical(df["sentiment"], **config.categorical.valence),
-            outlet=df["country"] + ":" + df["name"],
-            year=df["year"].astype(str),
-            month=df["month"].astype(str),
-            day=df["day"].astype(str),
-        ).pipe(
-            lambda df: df.assign(
-                time=df["country"] + ":" + df["year"] + ":" + df["month"] + ":" + df["day"],
-                country=pd.Categorical(
-                    df["country"],
-                    categories=list(config.categorical.country),
-                ),
-            )
-        )
-    )
-    .convert_dtypes()
+data = DataFrame.from_(paths.final).assign(
+    country=lambda df: pd.Categorical(
+        df["country"], categories=[*config.categorical.country]
+    ),
+    event=lambda df: pd.Categorical(
+        df["event"],
+        categories=[*config.categorical.event],
+        ordered=True,
+    ),
+    sentiment=lambda df: pd.Categorical(
+        df["sentiment"],
+        categories=[*config.categorical.sentiment],
+        ordered=True,
+    ),
+    valence=lambda df: pd.Categorical(
+        df["valence"],
+        categories=[*config.categorical.valence],
+        ordered=True,
+    ),
+)[["key", target, *opts.predictors.fixed, *opts.predictors.groups]]
+
+# %% ---------------------------------------------------------------------------------
+
+if (n := opts.model.get("subsample")) and n > 0:
+    print(f"Subsampling to {n} data points for faster model fitting...")
+    model_data = data.sample(n=n, random_state=rng)
+else:
+    model_data = data
+
+# %% ---------------------------------------------------------------------------------
+
+print(f"Fitting GLMM for '{target}' target with {model_data.shape[0]} observations")
+
+kwargs = dict(OmegaConf.to_object(opts.solver))
+kwargs["control"] = ro.ListVector(kwargs.get("control", {}))
+
+model = brm(
+    formula=opts.model.formula.format(target=target),
+    data=model_data,
+    prior=opts.model.get("prior"),
+    family=ro.StrVector([opts.model.family, opts.model.link]),
+    seed=int(rng.integers(0, 2**16 - 1)),
+    **kwargs,
 )
 
 # %% ---------------------------------------------------------------------------------
 
-model = bmb.Model(
-    formula=opts.formula.format(target=target),
-    data=data,
-    family=opts.family,
-    priors=make_priors(opts.priors),
-)
-model.build()
-
-# %% ---------------------------------------------------------------------------------
-
-fit_opts = opts.fit.copy()
-advi = model.fit(
-    callbacks=[fit_opts.pop("callback").make()],
-    random_seed=rng.integers(0, 2**32 - 1),
-    **fit_opts,
-)
-
-# %% ---------------------------------------------------------------------------------
-
-trace = advi.sample(random_state=rng, **opts.sample)
-idata = advi_trace_to_inference(trace, model=model)
-
-# %% ---------------------------------------------------------------------------------
-
-model.predict(idata, kind="response", inplace=True, random_seed=rng)
-
-# %% ---------------------------------------------------------------------------------
-
-idata.to_netcdf(output_dir / f"{target}.nc")
+print("Saving fitted 'brms' model as RDS file...")
+ro.r["saveRDS"](model.r, str(dirpath / f"{target}-model.rds"))
 
 # %% ---------------------------------------------------------------------------------
