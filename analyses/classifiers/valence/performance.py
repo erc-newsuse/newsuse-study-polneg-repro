84c5a483afe4a8e19d51f44aac6509e293abc943
Szymon Talaga <stalaga@protonmail.com>
2026-01-27 11:45:43 +0100

    Fix.

--- analyses/classifiers/valence/performance.py
+++ analyses/classifiers/valence/performance.py
@@ -60,9 +60,10 @@ pipe = pipeline("text-multi-classification", model=model, tokenizer=tokenizer)
 
 testing = pd.concat(
     [
-        dataset["test"].to_pandas().set_index(["ground_truth", "country", "key"]),
+        dataset["test"].to_pandas(),
+        # dataset["valid"].to_pandas(),
     ]
-)
+).set_index(["ground_truth", "country", "key"])
 
 results = DataFrame(
     tqdm(pipe(KeyDataset(testing, "text"), batch_size=16), total=len(testing)),
@@ -146,50 +147,71 @@ print(
 
 # %% Validation on ground truth labels -----------------------------------------------
 
-ground_truth = data.xs(True, level="ground_truth")
-
-# %% ---------------------------------------------------------------------------------
-
-perf_gt = ground_truth.groupby(level="target").apply(compute_metrics)
-# perf_gt_country = ground_truth.groupby(["country", "target"]).apply(compute_metrics)
-
-# %% Sample ground truth examples with machine labels --------------------------------
-
-content = (
-    # data.xs(True, level="ground_truth")
-    data.unstack("target")
-    .swaplevel(0, 1, axis=1)
-    .sort_index(axis=1)
+ground_truth = (
+    pd.concat(
+        {
+            split: ds.to_pandas()
+            for split, ds in dataset.filter(lambda x: x["ground_truth"]).items()
+        },
+        names=["split"],
+    )
+    .reset_index("split")
+    .reset_index(drop=True)
+    .set_index(["split", "country", "key"])
+    .sort_index()
+)
+labels = (
+    DataFrame(
+        tqdm(
+            pipe(KeyDataset(ground_truth, "text"), batch_size=16), total=len(ground_truth)
+        ),
+    )
     .pipe(
         lambda df: pd.concat(
-            [df[t].rename(columns={"pred": f"{t}_p", "true": f"{t}_t"}) for t in targets],
+            [
+                DataFrame(df[t].tolist()).rename(
+                    columns={"label": t, "score": f"{t}_score"}
+                )
+                for t in targets
+            ],
             axis=1,
         )
     )
-    .reset_index()
-    .merge(DataFrame.from_(paths.text), on="key")
-    .set_index(["country", "key"])
+    .set_index(ground_truth.index)
 )
 
 # %% ---------------------------------------------------------------------------------
 
-examples = (
-    content.loc["us"][["event_p", "sentiment_p", "fulltext"]]
-    .rename(columns={"event_p": "event", "sentiment_p": "sentiment", "fulltext": "text"})
-    .groupby(["event", "sentiment"])
-    .sample(3, random_state=303)
-    .reset_index(drop=True)
-    .set_index(["event", "sentiment"])
+data_gt = (
+    labels[["event", "sentiment"]]
+    .merge(
+        ground_truth[["event", "sentiment"]],
+        left_index=True,
+        right_index=True,
+        suffixes=("", "_t"),
+    )
+    .pipe(
+        lambda df: pd.concat(
+            {
+                t: df[[t, f"{t}_t"]].rename(columns={t: "pred", f"{t}_t": "true"})
+                for t in targets
+            },
+            axis=1,
+            names=["target"],
+        )
+    )
+    .stack(level=0, future_stack=True)
+    .swaplevel("key", "target")
     .sort_index()
 )
 
 # %% ---------------------------------------------------------------------------------
 
-print(
-    examples.style.format(escape="latex").to_latex(
-        hrules=True,
-        multirow_align="t",
-    )
-)
+perf_gt = data_gt.loc[["valid"]].groupby(level="target").apply(compute_metrics)
+perf_gt.head()
+
+# %% ---------------------------------------------------------------------------------
+
+data_gt.groupby(["country", "target"]).apply(compute_metrics)
 
 # %% ---------------------------------------------------------------------------------
